c0ff035e6ef22ecac6ad3ab304693ce13c993f26
Anup Pradhan <anuppradhan929@gmail.com>
2025-09-13 19:37:43 +0530

    soem erroor

--- PROJECT/notes-saver/src/components/Paste.jsx
+++ PROJECT/notes-saver/src/components/Paste.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { useState } from 'react'
+
 
 function Paste() {
+
+  const pastes = useSelector(state => state.pastes);
+  const dispatch = useDispatch();
+  const [search, setSearch] = useState('');
+  
+  const filteredData = pastes.filter(
+    (paste) => paste.title
+    .toLowerCase()
+    .includes(search.toLowerCase())
+  );
+
   return (
-    <div>Paste</div>
+    <div>
+
+    </div>
   )
 }
 
